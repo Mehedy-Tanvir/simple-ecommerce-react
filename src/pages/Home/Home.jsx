f72683227aea189ec8885449b8392517aa9abf74
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-02-15 13:16:02 +0600

    search functionality added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,66 @@
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../providers/AuthProvider";
+import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import toast from "react-hot-toast";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchProduct, setSearchProduct] = useState("");
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => {
         setProducts(data.products);
+        setFilteredProducts(data.products);
       });
   }, []);
-  console.log(products);
-  const authInfo = useContext(AuthContext);
-  console.log(authInfo);
+  const handleSearch = () => {
+    const matchedProduct = products?.filter(
+      (product) => product?.title === searchProduct
+    );
+    if (matchedProduct.length > 0) {
+      setFilteredProducts(matchedProduct);
+    } else {
+      toast.error("Product not found...");
+      setFilteredProducts(products);
+    }
+  };
+
   return (
     <div className="bg-[#1111110D] py-10 px-4">
       <h1 className="mb-8 text-5xl font-bold text-center">Our Products</h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {products?.map((product, idx) => (
-          <ProductCard product={product} key={idx}></ProductCard>
-        ))}
+      <div className="flex flex-col items-center justify-center gap-10 mb-10">
+        <div className="flex items-center justify-center mt-4 join xl:justify-start">
+          <input
+            className="w-[150px] input input-bordered join-item h-[56px] font-work-sans lg:w-[194px] lg:text-[20px]"
+            placeholder="Enter name"
+            id="input-element"
+            onChange={(event) => setSearchProduct(event?.target?.value)}
+          />
+          <button
+            className="btn join-item rounded-lg bg-green-500 h-[56px] normal-case text-white hover:bg-green-400 font-semibold w-[80px] lg:w-[100px] lg:text-[20px]"
+            id="btn-apply"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+        <div className="form-control">
+          <input
+            type="range"
+            min={0}
+            max="100"
+            value="40"
+            className="range range-success"
+          />
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {filteredProducts?.map((product, idx) => (
+            <ProductCard product={product} key={idx}></ProductCard>
+          ))}
+        </div>
       </div>
     </div>
   );
